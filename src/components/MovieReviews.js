8f93d1315d7c57781b7e8fc8e1a2a5160269ed6e
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-01-29 17:41:27 -0500

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,27 @@
-// Code MovieReviews Here
+import React from 'react'
+
+function genMovies(movies) {
+    // console.log('generating movies')
+    if (movies) {
+        return movies.map(m => {
+            return (
+                <div className='review'>
+                    <h2>Movie: {m.display_title}</h2>
+                    <h3>{m.byline}</h3>
+                    <h2>Title: {m.headline}</h2>
+                    <p>Summary: {m.summary_short}</p>
+                </div>
+            )
+        })
+    }
+}
+
+function MovieReviews(props) {
+    return (
+        <div className='review-list'>
+            {genMovies(props.reviews)}
+        </div>
+    )
+}
+
+export default MovieReviews
